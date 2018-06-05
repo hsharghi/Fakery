7a77826372e4dd5b47005e07a70858c2cae5fefa
Hadi Sharghi <hsharghi@gmail.com>
2018-06-05 18:42:11 +0430

    Using DateComponets() to generate new date

--- Sources/Fakery/Generators/Date.swift
+++ Sources/Fakery/Generators/Date.swift
@@ -3,14 +3,14 @@ import Foundation
 public final class FakeDate {
     
     public func forward(seconds: Int = 0, minutes: Int = 0, hours: Int = 0, days: Int = 0, months: Int = 0, years: Int = 0) -> Date {
-        var date = Calendar.current.date(byAdding: .second, value: seconds, to: Date()) ?? Date()
-        date = Calendar.current.date(byAdding: .minute, value: minutes, to: date) ?? Date()
-        date = Calendar.current.date(byAdding: .hour, value: hours, to: date) ?? Date()
-        date = Calendar.current.date(byAdding: .day, value: days, to: date) ?? Date()
-        date = Calendar.current.date(byAdding: .month, value: months, to: date) ?? Date()
-        date = Calendar.current.date(byAdding: .year, value: years, to: date) ?? Date()
-        
-        return date
+        var dateComponents = DateComponents()
+        dateComponents.second = seconds
+        dateComponents.minute = minutes
+        dateComponents.hour = hours
+        dateComponents.day = days
+        dateComponents.month = months
+        dateComponents.year = years
+        return Calendar.current.date(byAdding: dateComponents, to: Date()) ?? Date()
     }
     
     public func backward(seconds: Int = 0, minutes: Int = 0, hours: Int = 0, days: Int = 0, months: Int = 0, years: Int = 0) -> Date {
